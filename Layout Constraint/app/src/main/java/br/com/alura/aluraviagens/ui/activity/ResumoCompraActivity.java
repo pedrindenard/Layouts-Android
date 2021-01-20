460aa92f10788fdb85b9d48f5fb10b0b6d1e20da
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-20 08:33:29 -0300

    Criando activity parabenização

--- Layout Constraint/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
+++ Layout Constraint/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -16,28 +16,43 @@ import br.com.alura.aluraviagens.util.ResourceUtil;
 
 public class ResumoCompraActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Resumo da compra";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_compra);
 
-        setTitle("Resumo da compra");
+        setTitle(TITULO_APPBAR);
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.resumo_compra_local_pacote);
-        local.setText(pacoteSaoPaulo.getLocal());
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+    }
 
-        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
-        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
-        imagem.setImageDrawable(drawableDoPacote);
-
-        TextView data = findViewById(R.id.resumo_compra_data_viagem);
-        String periodoEmTexto = DataUtil.periodoEmTexto(pacoteSaoPaulo.getDias());
-        data.setText(periodoEmTexto);
-
+    private void mostraPreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
-        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
         preco.setText(moedaBrasileira);
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacote.getLocal());
     }
 }
